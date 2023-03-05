2f1e7178b8822d70e4f9ce3bdcc3ad7b5cf72774
Pedro Henrique GOdri <pedrogodri@gmail.com>
2023-03-05 17:03:29 -0300

    fix: recomeçando

--- valorant-information/src/app/app.component.spec.ts
+++ valorant-information/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'valorant-informations'`, () => {
+  it(`should have as title 'valorant-information'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('valorant-informations');
+    expect(app.title).toEqual('valorant-information');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('valorant-informations app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('valorant-information app is running!');
   });
 });
